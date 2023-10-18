19d4bda8b4734a624f9fdb5f28594ec73ce59679
fernandoferra <fernandofaugusto@gmail.com>
2023-10-18 12:25:40 -0300

    primeiro projeto conta bancaria

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,26 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+
+        double saldo = 237.48;
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Por favor, digite o número da Agência !");
+        String agencia = scanner.next();
+
+        System.out.println("Por favor, digite o número da sua conta !");
+        int numeroConta = scanner.nextInt();
+
+        System.out.println("Por favor, digite seu nome !");
+        String nome = scanner.next();
+
+        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " 
+        + agencia + ", conta " + numeroConta + " e seu saldo " + saldo + " já está disponível para saque.");
+
+        scanner.close();
     }
 }
